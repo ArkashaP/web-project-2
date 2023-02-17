9050035d0f066eac9640659418887590190cd918
Ark <arkasha960@gmail.com>
2023-02-17 17:58:19 +0300

    Lab 1 fixes + Lab 2 created

---
+++ labs/lab_2/task_1/main.js
@@ -0,0 +1,8 @@
+var button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+
+});
+
+const a = [0 ,2]
+const b = a.copyWithin()
--- main.js
+++ main.js
@@ -10,18 +10,3 @@ async function delayedLoop() {
         //getSelection(first_p).modify();
     };
 };
-
-
-
-// #2
-
-t_sides = [3, 2, 2];
-if ((t_sides[0]+t_sides[1]>t_sides[2]) && (t_sides[1]+t_sides[2]>t_sides[0]) && (t_sides[2]+t_sides[0]>t_sides[1])){
-    console.log('Не существует');
-} else{
-    console.log('Cуществует');
-}
-
-t_p = (t_sides[0] + t_sides[1] + t_sides[2])/3
-console.log(`Периметр: ${t_p}`);
-console.log(`dsd: ${ Math.sqrt( t_p * (t_p-t_sides[0]) * (t_p-t_sides[1]) * (t_p-t_sides[2]) )}`);
