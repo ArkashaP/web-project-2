1ed03e7d3df4d52f1a1ffd93d9dd3dd045d47072
Ark <arkasha960@gmail.com>
2023-02-20 23:01:26 +0300

    Task 5 finished

--- labs/lab_2/task_5/main.js
+++ labs/lab_2/task_5/main.js
@@ -1,27 +1,44 @@
 const numList = document.getElementById('numList');
 const outputNum = document.getElementById('outputValue');
-const somekk = document.getElementById('listEl0');
+const amountNum = document.getElementById('amountEdit');
 
-
-
-function getRandArray(min, max) {
-    min = Number(min);
-    max = Number(max);
-    return isNaN(min) && isNaN(max) ? NaN : Math.random() * (max - min) + min;
+function swapNums(num1, num2){
+    
 }
 
-let numArray = [0];
+function getRandFloat(from, to){
+    from = Number(from);
+    to = Number(to);
+    return isNaN(from) && isNaN(to) ? NaN : Math.random() * (to - from) + from;
+
+}
+function getRandArray(array, amount) {
+    let res_array = [];
+    for(i = 0; i < amount; i+=1){
+        res_array[i] = array[Math.floor(getRandFloat(0, array.length))];
+    }
+    return res_array;
+}
+
+let numArray = [];
 
 function delElement(){
-    document.getElementById(`listEl${numArray.length-1}`).remove();
+    if(numArray.length<=0) return;
     numArray.pop();
+    document.getElementById(`listEl${numArray.length}`).remove();
 }
 function addElement(){
-    numArray.push(0);
     numList.insertAdjacentHTML('beforeEnd',
-    `<li id="num${numArray.length-1}"><input type="number"></li>`);
+    `<li id="listEl${numArray.length}"><input type="number" id="numEl${numArray.length}"></li>`);
+    numArray.push(0);
 }
 
 function onSubmit() {
-    somekk.remove();
+    outputNum.value = "";
+    
+    for(i = 0; i < numArray.length; i+=1){
+        numArray[i] = document.getElementById(`numEl${i}`).value;
+    }
+    outputNum.value=getRandArray(numArray, amountNum.value)
 }; 
+
