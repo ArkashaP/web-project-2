a2e9e26a16fbf1c158cbe2ac0c80992dce4ef3ba
Ark <arkasha960@gmail.com>
2023-05-26 15:15:18 +0300

    Somte changes

--- labs/lab_6-8/src/routes/db.comments.routes.js
+++ labs/lab_6-8/src/routes/db.comments.routes.js
@@ -4,11 +4,14 @@ const controller = require('../controllers/db.comments.controller')
 
 
 router.get('/ping', controller.ping)
+
 router.get('/', controller.getComments)
+router.post('/', controller.postComment)
+
 router.get('/:id', controller.getComment)
 router.put('/:id', controller.updateComment)
 router.delete('/:id', controller.deleteComment)
-router.post('/', controller.postComment)
+
 
 
 module.exports = router;
