f8a3f808bc533ca8ab12325e5d68fa19fb4c2f9c
Ark <arkasha960@gmail.com>
2023-06-12 14:18:07 +0300

    js files update

--- labs/lab_9-12/src/controllers/db.models.controller.js
+++ labs/lab_9-12/src/controllers/db.models.controller.js
@@ -11,14 +11,13 @@ class ControllerDB{
 
     async postModel(req, res, next) {
         // const {comment, id} = req.
-
+        let name;
         await dbUsersService.findWithToken('Users', req.header('api_key')).then(result=>{
-
+            name = result['name']
         }).catch(error=>{
             console.log(error);
             next(error);
         });
-        console.log(name)
 
         const {model_name, type_of_model, object_model, description, comments} = req.body;
         const creation_date = new Date();
@@ -44,11 +43,16 @@ class ControllerDB{
     }
     async getModels(req, res, next) {
         dbService.findAll(collectionName).then(result=>{
-            res.json(result);
+            if(result.length == 0) {
+                const err = new Error('No models found! (DB is empty)');
+                // TODO: Status code!
+                next(err)
+            } else{
+                res.json(result);
+            }
         }).catch(error=>{
             console.log(error);
-            // TODO: Добавить обработчик ошибок
-            next();
+            next(error);
         });
 
     }
@@ -57,9 +61,14 @@ class ControllerDB{
     async getModel(req, res, next) {
         // TODO: Владелец видит всю инфу
         const id = req.params.id;
-        // const {id} = req.body;
         dbService.findOne(collectionName, id).then(result=>{
-            res.json(result);
+            if(!result){
+                const err = new Error('Model not found! (Wrong id)');
+                err.status = 404; // TODO: Status!
+                next(err);
+            }else{
+                res.json(result);
+            }
         }).catch(error=>{
             next(error);
             console.log(error);
@@ -68,7 +77,21 @@ class ControllerDB{
     }
     async updateModel(req, res, next) {
         const id = req.params.id;
-        dbService.update(collectionName, id, req.body).then(result=>{
+
+        const modified_date = new Date(); // Update date
+        
+        // {
+        //     name,
+        //     model_name,
+        //     type_of_model,
+        //     object_model,
+        //     description,
+        //     comments,
+        //     creation_date,
+        //     modified_date
+        // } // TODO: Do checks for these keys
+        const result = Object.assign({}, req.body, {modified_date}); 
+        dbService.update(collectionName, id, result).then(result=>{
             res.json(result);
         }).catch(error=>{
             console.log(error);
